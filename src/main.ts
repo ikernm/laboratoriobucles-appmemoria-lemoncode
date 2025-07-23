9adfcf41e9c411fbb55b83a28c90d5b2c0ef8b9e
ikernm <ikernieblamoreno@hotmail.com>
2025-07-23 19:55:09 +0200

    prueba de concepto 2

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,30 @@
 import "./style.css";
 
+const tarjetaImagen = document.getElementById("real-card") as HTMLImageElement;
+
+const cartas = ['🦁','🦉','🐶','🐔','🐷','🐝'];
+
+const numeroAleatorioCarta = (cartas: number): number => {
+    return Math.floor(Math.random() * cartas);
+}
+
+function barajarCartas(cartas: string[]): string[] {
+    for (let i= cartas.length - 1; i > 0; i--) {
+        const c = numeroAleatorioCarta(i+1);
+        [[cartas[i], cartas[c]] = [cartas[c], cartas[i]]];
+    }
+    return cartas;
+}
+
+console.log(barajarCartas(cartas));
+
+const mostrarCarta = (carta:string) => {
+    tarjetaImagen.src = '/images/' + numeroAleatorioCarta(cartas.length) + '.png';
+    tarjetaImagen.alt = carta;
+}
+
+tarjetaImagen.addEventListener('click', () => {
+    const cartaAleatoria = cartas[Math.floor(Math.random() * cartas.length)];
+    tarjetaImagen.src = '/images/' + cartaAleatoria + '.png';
+    mostrarCarta(cartaAleatoria);
+});
